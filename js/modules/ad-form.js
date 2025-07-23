9619440c33af07c3db0ab337125a4896d829f231
Arseniy <aenadegnow@gmail.com>
2025-07-23 11:25:16 +0500

    Правда или действие №1

--- js/modules/ad-form.js
+++ js/modules/ad-form.js
@@ -3,6 +3,12 @@ const adFormInputs = adForm.querySelectorAll('input, select');
 const mapFiltersForm = document.querySelector('.map__filters');
 const mapFiltersFormInputs = mapFiltersForm.querySelector('input, select');
 
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
+  errorTextClass: 'ad-form__element--wide'
+});
+
 function toInactiveStatus() {
   adForm.classList.add('ad-form--disabled');
   for (const adFormInput of adFormInputs) {
@@ -26,3 +32,33 @@ function toActiveStatus() {
   }
 }
 toActiveStatus();
+
+// Валидация
+
+const roomsField = adForm.querySelector('[name="rooms"]');
+const guestsField = adForm.querySelector('[name="capacity"]');
+const guestsInRooms = {
+  '1': ['0', '1'],
+  '2': ['0', '1', '2'],
+  '3':['0', '1', '2', '3'],
+  '100': ['0']
+};
+
+function validateGuestsINRooms() {
+  return guestsInRooms[roomsField.value].includes(guestsField.value);
+}
+
+function guestsInRoomsErrorMessage() {
+  if (roomsField.value === '100' && guestsField.value !== '0') {
+    return '100 комнат не предназначены для гостей';
+  }
+  return `Невозможно разместить ${guestsField.value} гостей в ${roomsField.value} комнатах.`;
+}
+
+pristine.addValidator(roomsField, validateGuestsINRooms, guestsInRoomsErrorMessage);
+pristine.addValidator(guestsField, validateGuestsINRooms, guestsInRoomsErrorMessage);
+
+adForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
